28cf58ea8222847bf4178843cfde2f3460bcd1cc
Ale26rangers <alessandro.rabaglio@gmail.com>
2026-06-05 11:35:08 +0200

    Update app.py

--- app.py
+++ app.py
@@ -96,9 +96,9 @@ if controlla_password():
         st.success("🟢 **Sistema Offline:** Caricamento istantaneo e sicuro.")
         
         # 🟢 MODIFICA QUI I NUMERI DEL LOTTO SVIZZERO QUANDO VUOI AGGIORNARLI
-        caldi = [18, 32, 22, 38, 26, 11]  # Inserisci i 6 numeri più estratti separati da virgola
+        caldi = [36, 5, 40, 31, 6, 24]  # Inserisci i 6 numeri più estratti separati da virgola
         freddi = [42, 9, 28, 14, 30, 36]  # Inserisci i 6 numeri ritardatari separati da virgola
-        l_caldi = [5, 2]                  # Inserisci i 2 Numeri Fortunati migliori
+        l_caldi = [1, 5, 2]                  # Inserisci i 2 Numeri Fortunati migliori
         # -------------------------------------------------------------------
 
         tab1, tab2 = st.tabs(["📊 Analisi Storica", "⚙️ Riduttore Matematico"])
@@ -143,9 +143,9 @@ if controlla_password():
         st.success("🟢 **Sistema Offline:** Caricamento istantaneo e sicuro.")
         
         # 🟢 MODIFICA QUI I NUMERI DELL'EUROMILLIONS QUANDO VUOI AGGIORNARLI
-        e_caldi = [23, 44, 50, 19, 37]  # Inserisci i 5 numeri più estratti separati da virgola
+        e_caldi = [23, 44, 42, 19, 21]  # Inserisci i 5 numeri più estratti separati da virgola
         e_freddi = [33, 41, 13, 48, 22] # Inserisci i 5 numeri ritardatari separati da virgola
-        s_calde = [3, 2]                # Inserisci le 2 stelle più frequenti
+        s_calde = [2, 3, 8]                # Inserisci le 2 stelle più frequenti
         # -------------------------------------------------------------------
 
         tab1, tab2 = st.tabs(["📊 Analisi Storica", "⚙️ Riduttore Matematico"])
